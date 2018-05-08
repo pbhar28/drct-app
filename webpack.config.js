6c9f324229c4c0c10c6cc4a9f15342ed14826d9a
Piyush Bhardwaj <piyush_bhardwaj@infosys.com>
2018-05-08 19:21:52 +0530

    Adding files 8 may

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+
+var config = {
+		mode: 'development',
+		entry: './src/main.js',
+
+		output: {
+			filename: 'index.js',
+			path: path.resolve(__dirname, 'build'),
+			publicPath: '/build/' 
+		},
+
+		devServer: {
+			contentBase: path.join(__dirname, "build"),
+			port: 3000
+		},
+
+		module: {
+			rules: [ 
+				{
+					test: /\.jsx?$/,
+					exclude: /node_modules/,
+					loader: 'babel-loader',
+					query: {
+						presets: ['es2015', 'react']
+					}
+				},
+				{
+					test: /\.css$/,
+					use: [ 'style-loader', 'css-loader' ]
+				}
+			]
+		} 
+
+}
+
+module.exports = config;
